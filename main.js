5fcffa6b739806ddb307746f5874754855bb2706
Augustin C Benny <84260365+augustincbenny@users.noreply.github.com>
2023-04-09 12:15:29 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,57 @@
+$(document).ready(function(){
+
+    $('.fa-bars').click(function(){
+      $(this).toggleClass('fa-times');
+      $('.navbar').toggleClass('nav-toggle');
+    });
+  
+    $(window).on('scroll load',function(){
+  
+      $('.fa-bars').removeClass('fa-times');
+      $('.navbar').removeClass('nav-toggle');
+  
+      if($(window).scrollTop() > 30){
+        $('header').addClass('header-active');
+      }else{
+        $('header').removeClass('header-active');
+      }
+  
+      $('section').each(function(){
+        var id = $(this).attr('id');
+        var height = $(this).height();
+        var offset = $(this).offset().top - 200;
+        var top = $(window).scrollTop();
+        if(top >= offset && top < offset + height){
+          $('.navbar ul li a').removeClass('active');
+          $('.navbar').find('[href="#' + id + '"]').addClass('active');
+        }
+      });
+  
+    });
+  
+  });
+  $(document).bind("contextmenu",function(e) {
+    e.preventDefault();
+  });
+  $(document).keydown(function(e){
+    if(e.which === 123){
+      return false;
+  }
+  });
+  document.onkeydown = function(e) {
+    if(event.keyCode == 123) {
+       return false;
+    }
+    if(e.ctrlKey && e.shiftKey && e.keyCode == 'I'.charCodeAt(0)) {
+       return false;
+    }
+    if(e.ctrlKey && e.shiftKey && e.keyCode == 'C'.charCodeAt(0)) {
+       return false;
+    }
+    if(e.ctrlKey && e.shiftKey && e.keyCode == 'J'.charCodeAt(0)) {
+       return false;
+    }
+    if(e.ctrlKey && e.keyCode == 'U'.charCodeAt(0)) {
+       return false;
+    }
+  }
